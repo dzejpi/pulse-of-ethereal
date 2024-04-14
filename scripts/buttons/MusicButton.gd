c556c63d914345f3a5ac289d90a5ebc825432b3e
DzejPi <dzejjpii@gmail.com>
2024-04-14 20:10:53 +0200

    Changed music button UI to be always on.

--- scripts/buttons/MusicButton.gd
+++ scripts/buttons/MusicButton.gd
@@ -8,13 +8,13 @@ onready var music_label = $"MusicLabel"
 func _ready():
 	if AudioServer.is_bus_mute(AudioServer.get_bus_index("Music")):
 		music_on = false
-		music_label.text = "Music: Off"
+		music_label.text = "Music always on!"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
 		pressed = false
 		release_focus()
 	else:
 		music_on = true
-		music_label.text = "Music: On"
+		music_label.text = "Music always on!"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
 		pressed = true
 		release_focus()
@@ -23,13 +23,13 @@ func _ready():
 func _on_MusicButton_pressed():
 	if music_on:
 		music_on = false
-		music_label.text = "Music: Off"
+		music_label.text = "Music always on!"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
 		pressed = false
 		release_focus()
 	else:
 		music_on = true
-		music_label.text = "Music: On"
+		music_label.text = "Music always on!"
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
 		pressed = true
 		release_focus()
