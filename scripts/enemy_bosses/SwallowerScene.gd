61fad854ecc2af0633398d4377a22659474dc7c1
DzejPi <dzejjpii@gmail.com>
2024-04-14 18:22:26 +0200

    Increased swallower's HP.

--- scripts/enemy_bosses/SwallowerScene.gd
+++ scripts/enemy_bosses/SwallowerScene.gd
@@ -10,7 +10,7 @@ var enemy_one = preload("res://scenes/enemies/ShootingEnemy.tscn")
 var enemy_two = preload("res://scenes/enemies/EnemySuicideDrone.tscn")
 var enemy_three = preload("res://scenes/enemies/EnemyRotating.tscn")
 
-var enemy_health = 1000
+var enemy_health = 1250
 var enemy_score = 10000
 
 var is_hidden = true
