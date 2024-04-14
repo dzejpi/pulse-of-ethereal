27ba1e3106c7927eee28dc5155448a976a068361
DzejPi <dzejjpii@gmail.com>
2024-04-14 17:40:01 +0200

    Pickups now move forward and have a limited life.

--- scripts/pickups/RocketPickup.gd
+++ scripts/pickups/RocketPickup.gd
@@ -1,6 +1,17 @@
 extends Spatial
 
 
+var pickup_life = 10
+
+
+func _process(delta):
+	if pickup_life > 0:
+		pickup_life -= 1 * delta
+		transform.origin.z += 1 * delta
+	else:
+		queue_free()
+
+
 func collect_pickup():
 	self.get_parent().get_parent().gain_rocket()
 	queue_free()
